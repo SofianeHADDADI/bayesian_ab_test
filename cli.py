de2e3c8190e287e6aebf084faf42b6ab4066de82
Erik Taubeneck <erik.taubeneck@gmail.com>
2016-07-15 17:10:06 -0400

    make pip installable

--- cli.py
+++ cli.py
@@ -13,31 +13,38 @@ Options:
   --beta=<b>          Select a beta [default: 1.0]
 
 """
+from __future__ import print_function
+
 from docopt import docopt
 from ab_test import probability_relative_effect, probability_absolute_effect
 
-arguements = docopt(__doc__)
 
-a_population = int(arguements['<a_population>'])
-b_population = int(arguements['<b_population>'])
-a_success = int(arguements['<a_success>'])
-b_success = int(arguements['<b_success>'])
-sample_size = int(arguements['--sample_size'])
-absolute_effect_size = float(arguements['--absolute_effect_size'])
-if arguements['--relative_effect_size']:
-    relative_effect_size = float(arguements['--effect_size'])
-else:
-    relative_effect_size = None
+def main():
+    arguements = docopt(__doc__)
 
-alpha = float(arguements['--alpha'])
-beta = float(arguements['--beta'])
+    a_population = int(arguements['<a_population>'])
+    b_population = int(arguements['<b_population>'])
+    a_success = int(arguements['<a_success>'])
+    b_success = int(arguements['<b_success>'])
+    sample_size = int(arguements['--sample_size'])
+    absolute_effect_size = float(arguements['--absolute_effect_size'])
+    if arguements['--relative_effect_size']:
+        relative_effect_size = float(arguements['--effect_size'])
+    else:
+        relative_effect_size = None
+
+    alpha = float(arguements['--alpha'])
+    beta = float(arguements['--beta'])
+
+    if relative_effect_size:
+        print(probability_relative_effect(
+            a_population, a_success, b_population, b_success,
+            alpha, beta, sample_size, relative_effect_size))
+    else:
+        print(probability_absolute_effect(
+            a_population, a_success, b_population, b_success,
+            alpha, beta, sample_size, absolute_effect_size))
 
 
-if relative_effect_size:
-    print probability_relative_effect(
-        a_population, a_success, b_population, b_success,
-        alpha, beta, sample_size, relative_effect_size)
-else:
-    print probability_absolute_effect(
-        a_population, a_success, b_population, b_success,
-        alpha, beta, sample_size, absolute_effect_size)
+if __name__ == "__main__":
+    main()
